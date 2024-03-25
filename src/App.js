2f8240cd296b134be971e3b6484c097d8bc0d7c7
Nithin-kasturi <nithinkasturi8@gmail.com>
2024-03-25 13:19:58 +0530

    First commit in frontend

--- src/App.js
+++ src/App.js
@@ -1,94 +1,81 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
-
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { con } from './config';
+import './App.css';
+import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
+import Feed from "./Feed";
 export default function Home() {
+  const [currentAccount,setCurrentAccount]=useState('');
+  const [correctNetwork,setCorrectNetwork]=useState(false);
+  const connectMetamask=async()=>{
+    try{
+      if(!window.ethereum){
+        alert("Download and Install Metamask");
+        return;
+      } 
+      else{
+        const eth_chainId=await window.ethereum.request({method:'eth_chainId'});
+        if(eth_chainId!='0xaa36a7'){
+          alert("Connect to sepolia test network");
+          setCorrectNetwork(false);
+          return;
+        }
+        else{
+          setCorrectNetwork(true);
+        }
+      }
+      const accounts=await window.ethereum.request({method:'eth_requestAccounts'});
+      setCurrentAccount(accounts[0]);
+
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+  // Checks if wallet is connected to the correct network
+  const checkCorrectNetwork = async () => {
+    const { ethereum } = window
+    let chainId = await ethereum.request({ method: 'eth_chainId' })
+    console.log('Connected to chain:' + chainId)
+    if(chainId!='0xaa36a7'){
+      alert("Connect to sepolia test network");
+      setCorrectNetwork(false);
+      return;
+    }
+    else{
+      setCorrectNetwork(true);
+    }
+  }
+  useEffect(()=>{
+    connectMetamask();
+    checkCorrectNetwork();
+    
+  },[])
   return (
-    <main className="main">
-      <div className="container">
-        <div className="header">
-          <h1 className="title">
-            Welcome to{" "}
-            <span className="gradient-text-0">
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
-            <code className="code">src/App.js</code> file!
-          </p>
-
-          <div className="connect">
-            <ConnectWallet />
-          </div>
+    <div>
+    {currentAccount === '' ? (
+      <button
+      className='text-2xl font-bold py-3 px-12 bg-[#f1c232] rounded-lg mb-10 hover:scale-105 transition duration-500 ease-in-out'
+      onClick={connectMetamask}
+      >
+      Connect Wallet
+      </button>
+      ) : correctNetwork ? (
+        <div className="app">
+          <Sidebar />
+          <Feed />
+          <Widgets />
         </div>
-
-        <div className="grid">
-          <a
-            href="https://portal.thirdweb.com/"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-1">Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-2">Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-3">Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+      ) : (
+      <div className='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
+      <div>----------------------------------------</div>
+      <div>Please connect to the Sepolia Testnet</div>
+      <div>and reload the page</div>
+      <div>----------------------------------------</div>
       </div>
-    </main>
-  );
+    )}
+    </div>
+
+  )
 }
